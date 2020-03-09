dc4d65a66323709559ee21045cda7a2b9094a098
Bangara raju <raju.kottedi435@hotmail.com>
2020-03-09 14:41:39 +0300

    Add project files.

---
+++ BankOfDotNetAPI/Models/BankContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfDotNetAPI.Models
+{
+    public class BankContext : DbContext
+    {
+        public BankContext(DbContextOptions<BankContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
